7bb6ff4941863c6bdee6a730027813ea46c23a8a
wybielak <dawidnowakowski87@gmail.com>
2025-01-12 22:09:03 +0100

    router init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <p>Home</p>
+  },
+  {
+    path: '/dev',
+    element: <p>dev</p>
+  },{
+    path: '/craft',
+    element: <p>craft</p>
+  },
+  {
+    path: '/pizza',
+    element: <p>pizza</p>
+  }
+])
 
 function App() {
 
   return (
     <>
-      <p className="text-red-100">Test</p>
+      <RouterProvider router={router} />
     </>
   )
 }
