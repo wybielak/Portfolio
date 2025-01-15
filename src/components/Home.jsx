4b8e9283e79a2507792f906705e52cdf279ac892
wybielak <dawidnowakowski87@gmail.com>
2025-01-15 01:46:02 +0100

    visual adjustment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,21 +6,22 @@ export default function Home() {
     return (
         <>
             <div className="bg-gradient-darkbrown-olivegreen w-screen h-screen flex flex-col items-center justify-center">
-                <div className="bg-david bg-opacity w-smaller-screen md:w-smaller-screen-pc h-smaller-screen md:h-smaller-screen-pc border-0 rounded-3xl flex flex-col items-center justify-center">
-                    <h1 className="mt-6 md:m-0 text-6xl md:text-9xl font-bold text-center">
+                <div className="bg-david bg-opacity w-smaller-screen md:w-smaller-screen-pc h-smaller-screen md:h-smaller-screen-pc border-0 rounded-3xl flex flex-col items-center justify-between md:justify-center">
+                    <h1 className="m-2 text-5xl md:text-9xl font-bold text-center">
                         <span>Dawid Nowakowski</span>
+                        <p className="m-2 text-4xl md:text-8xl font-normal fade-in-anim">
+                            <span>p</span>
+                            <span>o</span>
+                            <span>r</span>
+                            <span>t</span>
+                            <span>f</span>
+                            <span>o</span>
+                            <span>l</span>
+                            <span>i</span>
+                            <span>o</span>
+                        </p>
                     </h1>
-                    <h1 className="mb-5 md:m-0 text-5xl md:text-8xl fade-in-anim">
-                        <span>p</span>
-                        <span>o</span>
-                        <span>r</span>
-                        <span>t</span>
-                        <span>f</span>
-                        <span>o</span>
-                        <span>l</span>
-                        <span>i</span>
-                        <span>o</span>
-                    </h1>
+
                     <Menu />
                 </div>
             </div>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom'
 export default function Menu() {
     return (
         <>
-            <div className='w-screen flex flex-col md:flex-row flex-nowrap items-center justify-around fade-in-slower-anim'>
-                <span><button type='button' className="text-3xl tracking-widest mt-4 md:m-0 transition hover:opacity-70 hover:scale-110"><NavLink to="/dev">DEV</NavLink></button></span>
-                <span><button type='button' className="text-3xl tracking-widest mt-4 md:m-0 transition hover:opacity-70 hover:scale-110"><NavLink to="/workshop">WORKSHOP</NavLink></button></span>
-                <span><button type='button' className="text-3xl tracking-widest mt-4 md:m-0 transition hover:opacity-70 hover:scale-110"><NavLink to="/pizza">PIZZA</NavLink></button></span>
+            <div className='m-6 w-screen flex flex-col md:flex-row flex-nowrap items-center justify-around fade-in-slower-anim'>
+                <span><button type='button' className="text-3xl tracking-widest mt-6 md:m-0 transition hover:opacity-70 hover:scale-110"><NavLink to="/dev">DEV</NavLink></button></span>
+                <span><button type='button' className="text-3xl tracking-widest mt-6 md:m-0 transition hover:opacity-70 hover:scale-110"><NavLink to="/workshop">WORKSHOP</NavLink></button></span>
+                <span><button type='button' className="text-3xl tracking-widest mt-6 md:m-0 transition hover:opacity-70 hover:scale-110"><NavLink to="/pizza">PIZZA</NavLink></button></span>
             </div>
         </>
     )
