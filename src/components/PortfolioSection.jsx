5923884cb3fa6faa93b0f2eada01747cadbb9f5a
wybielak <dawidnowakowski87@gmail.com>
2025-02-02 17:04:00 +0100

    gallery improvement

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -39,7 +39,7 @@ export default observer(function PortfolioSection({ title }) {
                     </p>
                 </div>
             </div>
-            <div className='w-halfscreen mb-16 -mt-16 columns-1 sm:columns-2 lg:columns-3 xl:columns-4'>
+            <div className='w-halfscreen px-4 mb-16 -mt-20 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
                 {projects.map((project, index) => (
                     <ProjectCard key={index} project={project} />
                 ))}
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,26 +6,39 @@ import { TbBrandCpp } from "react-icons/tb";
 export default function ProjectCard({ project }) {
 
   const [isLoaded, setIsLoaded] = useState(false)
+  const [imgDimensions, setImgDimensions] = useState({ width: 0, height: 0 });
+
+  const handleImageLoad = (e) => {
+    const { naturalWidth, naturalHeight } = e.target
+    setImgDimensions({ width: naturalWidth, height: naturalHeight })
+    setIsLoaded(true)
+  }
 
   return (
     <>
       {!isLoaded && <div className="h-60 w-full bg-gray-300 animate-pulse rounded-3xl"></div>}
 
-      <div className={`mb-4 relative border-0 rounded-3xl ${!isLoaded ? "hidden" : ""}`}>
-        <img className="object-cover border-0 rounded-3xl" src={project.photo} alt={project.name} onLoad={() => setIsLoaded(true)} />
+      <div className={`mb-4 relative border-0 rounded-3xl ${!isLoaded ? "hidden" : ""} ${imgDimensions.width > imgDimensions.height ? "sm:col-span-2" : ""} ${imgDimensions.height > imgDimensions.width ? "sm:row-span-2" : ""}`} >
+        
+        <img className="object-cover border-0 rounded-3xl w-full h-full" src={project.photo} alt={project.name} onLoad={handleImageLoad} />
 
         <div className="p-8 absolute inset-0 flex flex-col items-start justify-between bg-black bg-opacity-40 text-white text-center border-0 rounded-3xl">
+          
           <div className="w-full flex flex-row items-center justify-between">
+
             <h1 className="text-2xl font-bold">{project.date}</h1>
+
             <div className="flex flex-row flex-nowrap items-center justify-center gap-x-6 text-3xl">
               {project.github && <a href={project.github} target="_blank"><FaGithub /></a>}
               {project.youtube && <a href={project.youtube} target="_blank"><FaYoutube /></a>}
               {project.instagram && <a href={project.instagram} target="_blank"><FaInstagram /></a>}
               {project.tiktok && <a href={project.tiktok} target="_blank"><FaTiktok /></a>}
             </div>
+
           </div>
 
           <div className="w-full flex flex-col items-start justify-start gap-y-2">
+
             <div className="flex flex-row flex-nowrap items-center justify-center text-2xl">
               {project.category === "react" && <FaReact />}
               {project.category === "cpp" && <TbBrandCpp />}
@@ -33,10 +46,14 @@ export default function ProjectCard({ project }) {
               {project.category === "unity" && <FaUnity />}
               {project.category === "js" && <FaJs />}
             </div>
+
             <h1 className="text-2xl font-bold text-left">{project.name}</h1>
             <p className="text-md text-left">{project.description}</p>
+            
           </div>
+
         </div>
+
       </div>
     </>
   )
